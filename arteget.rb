5ad5048c8ceb1e77043aed726a2d780a4844609d
Trou <devel@syscall.eu>
2011-02-25 15:26:58 +0100

    multi dl qui marche

--- arteget.rb
+++ arteget.rb
@@ -42,8 +42,8 @@ def print_usage
 	puts "Usage : arteget [-v] (--best=NUM|--top=NUM)|URL|program"
 end
 
-def parse_xml(hc, xml_url, progname=nil)
-	xml_content = hc.get(xml_url).content
+def parse_xml(xml_url, progname=nil)
+	xml_content = $hc.get(xml_url).content
 	xml = Document.new(xml_content)
 	result = []
 	xml.root.each_element("video") do |e|
@@ -57,14 +57,14 @@ def parse_xml(hc, xml_url, progname=nil)
 	return result
 end
 
-def get_progs_urls(hc, progname)
+def get_progs_urls(progname)
 	if progname =~ /^http:/ then
 		log("Trying with URL")
 		return progname
 	end
 	log("Getting index")
 
-	index = hc.get('/fr/videos').content
+	index = $hc.get('/fr/videos').content
 	xml_url = index[/coverflowXmlUrl = "(.*)"/,1]
 
 	log(xml_url, LOG_DEBUG)
@@ -73,39 +73,63 @@ def get_progs_urls(hc, progname)
 	if $options[:best] then
 		bestnum = $options[:best]
 		log("Getting best #{bestnum} list page")
-		result = parse_xml(hc, xml_url+"?hash=/tv/coverflow/popular/bestrated/1/#{bestnum}/")
-		pp result
-		exit
+		result = parse_xml(xml_url+"?hash=/tv/coverflow/popular/bestrated/1/#{bestnum}/")
 	elsif $options[:top] then
 		topnum = $options[:top]
 		log("Getting top #{topnum} list page")
-		result = parse_xml(hc, xml_url+"?hash=/tv/coverflow/popular/mostviewed/1/#{topnum}/")
-		pp result
-		exit
+		result = parse_xml(xml_url+"?hash=/tv/coverflow/popular/mostviewed/1/#{topnum}/")
 	else
 		log("Getting list page")
-		result = parse_xml(hc, xml_url, progname)
-		pp result
-		exit
-		index_list = hc.get(xml_url).content
-		result = [index_list[/href="(.*\/#{progname}.*\.html)"/,1]]
-
-		if not result then
-			log("Could not find program in list, trying another way")
-			log("Getting program page")
-			result = index[/href="(.*\/#{progname}.*\.html)"/,1]
-			fatal("Could not find program page") if not result
-			prog_page = hc.get(result).content
-			prog_list_url = prog_page[/listViewUrl: "(.*)"/,1]
-			fatal("Cannot find list for program") if not prog_list_url
-
-			log("Getting list page")
-			prog_list = hc.get(prog_list_url).content
-			result = [prog_list[/href="(.*\.html)"/,1]]
-		end
-		fatal("Cannot find program at all") if not result
+		result = parse_xml(xml_url, progname)
 	end
+	fatal("Cannot find requested program(s)") if not result
 	return result
+end
+
+def dump_video(page_url, title, teaser)
+	log("Trying to get #{title}, teaser : \"#{teaser}\"")
+	vid_id = page_url[/-(.*)\./,1]
+	fatal("No video id in URL") if not vid_id
+	fatal("Already downloaded") if Dir["*#{vid_id}*"].length > 0 
+
+	log("Getting video page")
+	page_video = $hc.get(page_url).content
+	videoref_url = page_video[/videorefFileUrl = "http:\/\/videos.arte.tv(.*\.xml)"/,1]
+	player_url = page_video[/url_player = "(.*\.swf)"/,1]
+	log(videoref_url, LOG_DEBUG) 
+	log(player_url, LOG_DEBUG) 
+
+	log("Getting video XML desc")
+	videoref_content = $hc.get(videoref_url).content
+	log(videoref_content, LOG_DEBUG)
+	ref_xml = Document.new(videoref_content)
+	vid_lang_url = ref_xml.root.elements["videos/video[@lang='#{$options[:lang]}']"].attributes['ref']
+	vid_lang_url.gsub!(/.*arte.tv/,'')
+	log(vid_lang_url, LOG_DEBUG)
+
+	log("Getting #{$options[:lang]} #{$options[:qual]} video XML desc")
+	vid_lang_xml_url = $hc.get(vid_lang_url).content
+	vid_lang_xml = Document.new(vid_lang_xml_url)
+	rtmp_url = vid_lang_xml.root.elements["urls/url[@quality='#{$options[:qual]}']"].text
+	log(rtmp_url, LOG_DEBUG)
+
+	log("Dumping video : #{vid_id}.flv")
+	log("rtmpdump --swfVfy #{player_url} -o #{vid_id}.flv -r \"#{rtmp_url}\"", LOG_DEBUG)
+	fork do 
+		exec("rtmpdump", "-q", "--swfVfy", player_url, "-o", "#{vid_id}.flv", "-r", rtmp_url)
+	end
+	Process.wait
+	if $?.exited?
+		case $?.exitstatus
+			when 0 then
+				log("Video successfully dumped")
+			when 1 then
+				fatal("rtmpdump failed")
+			when 2 then
+				log("rtmpdump exited, trying to resume")
+				exec("rtmpdump", "-e", "-q", "--swfVfy", player_url, "-o", "#{vid_id}.flv", "-r", rtmp_url)
+		end
+	end
 end
 
 begin 
@@ -129,52 +153,9 @@ elsif ARGV.length == 1
 	progname=ARGV.shift
 end
 
-hc = HttpClient.new("videos.arte.tv")
-hc.allowbadget = true
+$hc = HttpClient.new("videos.arte.tv")
+$hc.allowbadget = true
 
-program_page_url = get_progs_urls(hc, progname)
-log(program_page_url, LOG_DEBUG)
-
-vid_id = program_page_url[/-(.*)\./,1]
-fatal("No video id in URL") if not vid_id
-fatal("Already downloaded") if Dir["*#{vid_id}*"].length > 0 
-
-log("Getting video page")
-page_video = hc.get(program_page_url).content
-videoref_url = page_video[/videorefFileUrl = "http:\/\/videos.arte.tv(.*\.xml)"/,1]
-player_url = page_video[/url_player = "(.*\.swf)"/,1]
-log(videoref_url, LOG_DEBUG) 
-log(player_url, LOG_DEBUG) 
-
-log("Getting video XML desc")
-videoref_content = hc.get(videoref_url).content
-log(videoref_content, LOG_DEBUG)
-ref_xml = Document.new(videoref_content)
-vid_lang_url = ref_xml.root.elements["videos/video[@lang='#{$options[:lang]}']"].attributes['ref']
-vid_lang_url.gsub!(/.*arte.tv/,'')
-log(vid_lang_url, LOG_DEBUG)
-
-log("Getting #{$options[:lang]} #{$options[:qual]} video XML desc")
-vid_lang_xml_url = hc.get(vid_lang_url).content
-vid_lang_xml = Document.new(vid_lang_xml_url)
-rtmp_url = vid_lang_xml.root.elements["urls/url[@quality='#{$options[:qual]}']"].text
-log(rtmp_url, LOG_DEBUG)
-
-log("Dumping video : #{vid_id}.flv")
-log("rtmpdump --swfVfy #{player_url} -o #{vid_id}.flv -r \"#{rtmp_url}\"", LOG_DEBUG)
-fork do 
-	exec("rtmpdump", "-q", "--swfVfy", player_url, "-o", "#{vid_id}.flv", "-r", rtmp_url)
-end
-Process.wait
-if $?.exited?
-	case $?.exitstatus
-		when 0 then
-			log("Video successfully dumped")
-		when 1 then
-			fatal("rtmpdump failed")
-		when 2 then
-			log("rtmpdump exited, trying to resume")
-			exec("rtmpdump", "-e", "-q", "--swfVfy", player_url, "-o", "#{vid_id}.flv", "-r", rtmp_url)
-	end
-end
-
+progs_data = get_progs_urls(progname)
+#log(progs_data, LOG_DEBUG)
+progs_data.each {|p| dump_video(p[0], p[1], p[2]) }
